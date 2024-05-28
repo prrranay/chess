1eb5b99574780df880a8aa83162ca1ad59fa5413
prrranay <pkd4876308@gmail.com>
2024-05-28 18:09:58 +0530

    re check

--- index.js
+++ index.js
@@ -16,64 +16,64 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine', 'ejs'); 
 
 app.get('/', function(req, res){
-    res.send("hello world!");   
+    res.render('index');   
 })
 
-// io.on("connection", function(uniquesocket){
-//     console.log("connected");
-//     if(!players.white){
-//         players.white=uniquesocket.id;
-//         uniquesocket.emit("playerRole","w");
-//     }else if(!players.black){
-//         players.black=uniquesocket.id;
-//         uniquesocket.emit("playerRole","b");
-//     }else{
-//         uniquesocket.emit("spectatorRole")
-//     }
+io.on("connection", function(uniquesocket){
+    console.log("connected");
+    if(!players.white){
+        players.white=uniquesocket.id;
+        uniquesocket.emit("playerRole","w");
+    }else if(!players.black){
+        players.black=uniquesocket.id;
+        uniquesocket.emit("playerRole","b");
+    }else{
+        uniquesocket.emit("spectatorRole")
+    }
 
-//     uniquesocket.on("disconnect",function(){
-//         if(players.white==uniquesocket.id){
-//             players.white=null;
-//         }else if(players.black==uniquesocket.id){
-//             players.black=null;
-//         } 
-//     })
+    uniquesocket.on("disconnect",function(){
+        if(players.white==uniquesocket.id){
+            players.white=null;
+        }else if(players.black==uniquesocket.id){
+            players.black=null;
+        } 
+    })
 
-//     uniquesocket.on("move",(move)=>{
-//         try {
-//             if(chess.turn() == 'w' && uniquesocket.id !== players.white) {
-//                 uniquesocket.emit("whiteMove",function(){
-//                     console.log("return at white");
-//                     return;
-//                 })
-//             };
-//             if(chess.turn() == 'b' && uniquesocket.id !== players.black) {
-//                 uniquesocket.emit("blackMove",function(){
-//                     console.log("return at black");
-//                     return;
-//                 })
-//             };
+    uniquesocket.on("move",(move)=>{
+        try {
+            if(chess.turn() == 'w' && uniquesocket.id !== players.white) {
+                uniquesocket.emit("whiteMove",function(){
+                    console.log("return at white");
+                    return;
+                })
+            };
+            if(chess.turn() == 'b' && uniquesocket.id !== players.black) {
+                uniquesocket.emit("blackMove",function(){
+                    console.log("return at black");
+                    return;
+                })
+            };
             
-//             const result=chess.move(move);
-//             if(result){
-//                 currentPlayer=chess.turn();
-//                 io.emit("move",move)
-//                 io.emit("boadState",chess.fen())
-//                 if(chess.turn() == "w"){
-//                     uniquesocket.emit("whiteMove")
-//                 }else if(chess.turn() == "b"){
-//                     uniquesocket.emit("blackMove")
-//                 }
-//             }else{
-//                 console.log("invalid move",move)
-//                 uniquesocket.emit("invalidMove",move)
-//             }
-//         } catch (error) {
-//             console.log(error);
-//             uniquesocket.emit("invalidMove",move)
-//         }
-//     })
-// })
+            const result=chess.move(move);
+            if(result){
+                currentPlayer=chess.turn();
+                io.emit("move",move)
+                io.emit("boadState",chess.fen())
+                if(chess.turn() == "w"){
+                    uniquesocket.emit("whiteMove")
+                }else if(chess.turn() == "b"){
+                    uniquesocket.emit("blackMove")
+                }
+            }else{
+                console.log("invalid move",move)
+                uniquesocket.emit("invalidMove",move)
+            }
+        } catch (error) {
+            console.log(error);
+            uniquesocket.emit("invalidMove",move)
+        }
+    })
+})
 
 server.listen(3000,function(){
     console.log("Server is running on port 3000")}); 
